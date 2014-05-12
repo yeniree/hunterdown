987548d1fe5da420a8e7758d7776f05516012072
Danny Boscan <dannyboscan@gmail.com>
2014-05-12 10:12:21 -0430

    prueba 3

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 <html lang="es">
 <head>
 	<title>HunterDown</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=no">
 </head>
 <body>
   <h1>Hola Mundo</h1>
